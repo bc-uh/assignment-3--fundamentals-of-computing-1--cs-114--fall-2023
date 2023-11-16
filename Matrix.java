9d7eb514aab7bce45dc09fdcb1b1ad8a7c36c5cf
bc-uh <chern@hartford.edu>
2023-11-15 20:34:19 -0500

    Add swap method

--- Matrix.java
+++ Matrix.java
@@ -5,4 +5,11 @@ public class Matrix {
     matrix = new int[size][size];
     System.out.println("Matrix dimensions: " + size + " x " + size);
   }
+
+  private void swap(int x1, int y1, int x2, int y2) {
+    int temp = matrix[x1][y1];
+    matrix[x1][y1] = matrix[x2][y2];
+    matrix[x2][y2] = temp;
 }
+}
+
