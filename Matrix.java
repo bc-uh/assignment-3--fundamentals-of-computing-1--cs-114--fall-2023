f5ac94446be17d960dc7da9bff919d2a4cd70e97
bc-uh <chern@hartford.edu>
2023-11-15 20:22:14 -0500

    Setup constructor

--- Matrix.java
+++ Matrix.java
@@ -1,2 +1,8 @@
 public class Matrix {
+  private int[][] matrix;
+
+  public Matrix(int size) {
+    matrix = new int[size][size];
+    System.out.println("Matrix dimensions: " + size + " x " + size);
+  }
 }
