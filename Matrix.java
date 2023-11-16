91a88e6a63a84b7f3821edbfbc844fff233003f0
bc-uh <chern@hartford.edu>
2023-11-15 20:44:12 -0500

    Add print matrix method

--- Matrix.java
+++ Matrix.java
@@ -10,6 +10,19 @@ public class Matrix {
     int temp = matrix[x1][y1];
     matrix[x1][y1] = matrix[x2][y2];
     matrix[x2][y2] = temp;
-}
-}
+  }
 
+  public void printMatrix() {
+    for (int row = 0; row < matrix.length; row++) {
+      for (int column = 0; column < matrix[row].length; column++) {
+        if (row + column == matrix.length - 1) {
+          System.out.print("\033[33m" + matrix[row][column] + " \033[0m");
+        } else {
+          System.out.print(matrix[row][column] + " ");
+        }
+      }
+      System.out.println();
+    }
+    System.out.println();
+  }
+}
